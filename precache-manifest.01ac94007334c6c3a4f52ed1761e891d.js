fc2856efba946ddf9fcd4a77a77fc662265e116d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-07 18:24:41 +0000

    Deploy koo6357/travis-test to github.com/koo6357/travis-test.git:gh-pages

---
+++ precache-manifest.01ac94007334c6c3a4f52ed1761e891d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520bf761f1d4d2de1d906a53e2ed087f",
+    "url": "/travis-test/index.html"
+  },
+  {
+    "revision": "8fe607d0e7a3cc1797e7",
+    "url": "/travis-test/static/js/2.8219a6d0.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/travis-test/static/js/2.8219a6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558a7f0745ba39a9aa15",
+    "url": "/travis-test/static/js/main.0f0cc3d7.chunk.js"
+  },
+  {
+    "revision": "045ae6cd18d2f7346caa",
+    "url": "/travis-test/static/js/runtime-main.656b32a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
