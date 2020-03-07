36c67177536a0fb1b9165d228d7f95925b95ab7d
koo6357 <koo6357@naver.com>
2020-03-08 02:46:25 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    
+`;
+
+const Header = styled.header`
+    border-bottom:1px solid #eee;
+    height: 70px;
+    display:flex;
+    align-items:center;
+    padding: 0 50px;
+    .logo {
+        margin-right: 150px;
+        img {
+          width: 60px;
+        }
+    }
+`;
+
+const Nav = styled.nav`
+    display:flex;
+    align-items:center;
+    a {
+      display:block;
+      padding: 20px;
+      margin: 0 20px;
+      font-size: 16px;
+      text-transform:uppercase;
+      font-weight: 500;
+      color:#333;
+      text-decoration:none;
+    }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+          <div className="logo">
+              <img src={logo} alt="logo" />
+          </div>
+
+          <Nav>
+              <a href="#!">Home</a>
+              <a href="#!">Task</a>
+              <a href="#!">Service</a>
+              <a href="#!">Connect</a>
+          </Nav>
+      </Header>
+
+
+
+    </Container>
   );
 }
 
